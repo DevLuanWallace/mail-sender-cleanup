1a2e92e7533881296079a2635f42e165efe1772f
Natã Pedro <natanpedrodasilva@gmail.com>
2025-09-07 12:00:00 -0300

    [refatoração] atualizando AppRoutes.tsx para incluir rota de autenticação

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,14 +1,13 @@
 import { Route, Routes } from 'react-router'
-import './App.css'
-import { Home } from '../pages/Home'
+import AuthPage from '../pages/AuthPage'
 import { Home2 } from '../pages/Home2/Home2'
 
 export const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/home" element={<Home />} />
-      <Route path="/home2" element={<Home2><h1>Conteudo Exemplo</h1></Home2>} />
+      <Route path="/" element={<h1>Conteudo Exemplo</h1>} />
+      <Route path="/login" element={<AuthPage />} />
+      <Route path="/inbox" element={<Home2 children={undefined} />} />
     </Routes>
   )
 }
